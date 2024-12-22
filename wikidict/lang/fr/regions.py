472dc72af48bc5613a9dfa213d6bf108fd4bc320
chopinesque <chopinesque@users.noreply.github.com>
2024-12-22 01:31:55 +0000

    Update locale-specific data

--- wikidict/lang/fr/regions.py
+++ wikidict/lang/fr/regions.py
@@ -214,6 +214,7 @@ regions = {
     "Finlande": "Finlande",
     "Fondachelli-Fantina": "Fondachelli-Fantina",
     "Fontainebleau": "Fontainebleau",
+    "Forez": "Forez",
     "Forêt d'Othe": "Forêt d'Othe",
     "Fougères": "Fougères",
     "Fouta-Toro": "Fouta-Toro",
@@ -684,7 +685,7 @@ regions = {
     "Île-de-France": "Île-de-France",
     "Îles d’Aran": "Îles d'Aran",
     "Îles-de-la-Madeleine": "Îles-de-la-Madeleine",
-}  # 679
+}  # 680
 # END
 
 # handled differently
